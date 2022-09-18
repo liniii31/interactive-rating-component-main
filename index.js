1bb96c1c8d47f960bbb8374f7da1ee8f1c8dd7e4
Shalini Anand <42630854+liniii31@users.noreply.github.com>
2022-09-18 20:27:14 +0530

    disabling button

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ let thanksDiv=document.getElementById("thanks-div");
 thanksDiv.style.display="none";
 let ratingButton = document.getElementsByClassName('rate');
 let submit = document.getElementsByClassName('button')[0];
+submit.disabled = true;
 let previous;
 let selected;
 for(rates of ratingButton){
     rates.addEventListener("click", function(e){
+        submit.disabled= false;
+
         if(previous!=undefined){
             previous.style.backgroundColor="rgb(59, 57, 57)";
             previous.style.color="hsl(217, 12%, 63%)";
